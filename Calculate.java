2744c0fff7f7b8f7e079a326b00bb387fe9117eb
n2313 <nadolenko@ukr.net>
2016-04-29 19:43:30 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,6 @@
+/*имя файла должно совпадать с именем класса а не то ошибка*/
+public class Calculate{
+	public static void main(String[] arg) {
+		System.out.print("calculate...");
+		}/*должно точно так писаться ато компилироваться буде а запускаться нет*/
+}
